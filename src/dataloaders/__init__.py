5a6fd566cd6273ace27fffc22e0b09d598f5a612
lequan2902 <benjamin.lequan@gmail.com>
2022-12-15 01:39:39 +0800

    Update dataloader and models module

--- src/dataloaders/__init__.py
+++ src/dataloaders/__init__.py
@@ -1,6 +1,8 @@
+"""dataloaders module
+"""
 from .base import BaseDataLoader
 from .mnist import MNISTDataLoader
 
 __all__ = ["MNISTDataLoader", "BaseDataLoader"]
 
-dataloaders = {"mnist": MNISTDataLoader}
+dataloaders = {"mnist": (MNISTDataLoader, (1, 28, 28))}
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,10 @@
+"""Models module
+"""
 from .vae import BaseVAE, DeepVAE, ConvVAE
 
-__all__ = [BaseVAE, DeepVAE, ConvVAE]
+__all__ = ["BaseVAE", "DeepVAE", "ConvVAE"]
+
+models = {"BaseVAE": BaseVAE,
+          "DeepVAE": DeepVAE,
+         # "ConvVAE": ConvVAE
+          }
