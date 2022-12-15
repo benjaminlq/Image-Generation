044a6ec456e2d9d9edda7953694634a82cd83a4b
benjaminlq <benjamin.lequan@gmail.com>
2022-12-15 16:22:20 +0800

    Enable ConvVAE

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,7 +4,4 @@ from .vae import BaseVAE, DeepVAE, ConvVAE
 
 __all__ = ["BaseVAE", "DeepVAE", "ConvVAE"]
 
-models = {"BaseVAE": BaseVAE,
-          "DeepVAE": DeepVAE,
-         # "ConvVAE": ConvVAE
-          }
+models = {"BaseVAE": BaseVAE, "DeepVAE": DeepVAE, "ConvVAE": ConvVAE}
