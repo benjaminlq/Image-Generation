6635a75af8ef1d6a0e15bd0bb9c0e9c8b8234a6d
benjaminlq <benjamin.lequan@gmail.com>
2022-12-17 17:54:48 +0800

    Update inference for streamlit app

--- src/deploy/inference.py
+++ src/deploy/inference.py
@@ -5,7 +5,9 @@ import random
 from glob import glob
 from pathlib import Path
 
+import torch
 from torchvision import datasets, transforms
+from torchvision.utils import make_grid
 
 import config
 import utils
@@ -13,7 +15,7 @@ from dataloaders import dataloaders
 from models import models
 
 
-class Inference:
+class InferVAE:
     """Inference Module"""
 
     def __init__(
@@ -39,7 +41,7 @@ class Inference:
             self.model_dict[model_name] = models[model_type](
                 input_size=input_size, hidden_size=hidden_size
             )
-            self.model_dict[model_name].eval()
+            self.model_dict[model_name].eval().to(config.DEVICE)
             utils.load_model(self.model_dict[model_name], model_path)
 
         print(self.model_dict.keys())
@@ -59,14 +61,28 @@ class Inference:
             with open(str(config.DEPLOY_PATH / "jsons" / f"{dataset}.json"), "r") as f:
                 self.index_dict[dataset] = json.load(f)
 
+    def sample_image(self, class_no: int, dataset: str) -> torch.tensor:
+        """Sample an image from a class of a dataset
+
+        Args:
+            class_no (int): Class Number
+            dataset (str): Dataset to sample from
+
+        Returns:
+            torch.tensor: Sampled Image
+        """
+        img_idx = random.choice(self.index_dict[dataset][str(class_no)])
+        target_img, _ = self.datasets[dataset][img_idx]
+        return target_img
+
     def reconstruction(
-        self, class_no: int, model_type: str, hidden_size: int, dataset: str
+        self, target_img: torch.tensor, model_type: str, hidden_size: int, dataset: str
     ):
         """Reconstruction Feature. Take a random image of a specified class from the dataset, generate lalent vector distribution and
         reconstruct using Decoder. Quality of reconstruction is based on model type and hidden size of latent vector.
 
         Args:
-            class_no (int): Class to sample images from.
+            target_img (torch.tensor): Target Image for reconstruction
             model_type (str): Model checkpoint to load for reconstruction process
             hidden_size (int): Number of dimensions (features) of Latent Vector Space
             dataset (str): Dataset used for inference
@@ -75,18 +91,95 @@ class Inference:
             Tuple(torch.tensor, torch.tensor): Target Image, Reconstructed Image
         """
         model_name = "_".join([model_type, str(hidden_size), dataset])
-        img_idx = random.choice(self.index_dict[dataset][str(class_no)])
-        target_img, _ = self.datasets[dataset][img_idx]
 
         recon_img, _, _ = self.model_dict[model_name](
             target_img.unsqueeze(0).to(config.DEVICE)
         )
 
-        return target_img, recon_img.squeeze(0).cpu()
+        return recon_img.squeeze(0).cpu()
+
+    def encode(
+        self, input_img: torch.tensor, model_type: str, hidden_size: int, dataset: str
+    ) -> torch.tensor:
+        """Encode sample image to latent distribution and sample a latent vector from the output distribution
+
+        Args:
+            input_img (torch.tensor): Input Image
+            model_type (str): Model Type
+            hidden_size (int): No of dimensions of latent subspace
+            dataset (str): Dataset Used
+
+        Returns:
+            torch.tensor: Sampled Latent Representation of the input image
+        """
+        model_name = "_".join([model_type, str(hidden_size), dataset])
+        mu, logvar = self.model_dict[model_name].encode(
+            input_img.unsqueeze(0).to(config.DEVICE)
+        )
+        hidden_vector = self.model_dict[model_name].reparameterize(mu, logvar)
+        return hidden_vector.squeeze(0)
+
+    def decode(
+        self,
+        hidden_vector: torch.tensor,
+        model_type: str,
+        hidden_size: int,
+        dataset: str,
+    ) -> torch.tensor:
+        """Reconstruct Latent Vector to image
+
+        Args:
+            hidden_vector (torch.tensor): Latent Vector representing an image in latent space
+            model_type (str): Model Type
+            hidden_size (int): No of dimensions of latent subspace
+            dataset (str): Dataset Used
+
+        Returns:
+            torch.tensor: Reconstructed Image
+        """
+        model_name = "_".join([model_type, str(hidden_size), dataset])
+        recon_img = self.model_dict[model_name].decode(
+            hidden_vector.unsqueeze(0).to(config.DEVICE)
+        )
+        return recon_img.squeeze(0).cpu()
+
+    def interpolate(
+        self,
+        first_img: torch.tensor,
+        second_img: torch.tensor,
+        model_type: str,
+        hidden_size: int,
+        dataset: int,
+    ) -> torch.tensor:
+        """Generate samples in-between 2 input images in latent space
+
+        Args:
+            first_img (torch.tensor): Input First Image
+            second_img (torch.tensor): Input Second Image
+            model_type (str): Model Type
+            hidden_size (int): No of dimensions of latent subspace
+            dataset (str): Dataset Used
+
+        Returns:
+            torch.tensor: Interpolated Images
+        """
+        first_hidden = self.encode(first_img, model_type, hidden_size, dataset)
+        second_hidden = self.encode(second_img, model_type, hidden_size, dataset)
+        intermediate_hiddens = [
+            first_hidden * (1 - w) + second_hidden * w
+            for w in torch.arange(0.1, 1, 0.1)
+        ]
+        intermediate_imgs = [
+            self.decode(hidden, model_type, hidden_size, dataset)
+            for hidden in intermediate_hiddens
+        ]
+        imgs = [first_img] + intermediate_imgs + [second_img]
+        return make_grid(imgs, nrow=len(imgs))
 
 
 if __name__ == "__main__":
-    infer_cls = Inference()
-    target_img, recon_img = infer_cls.reconstruction(3, "ConvVAE", 2, "mnist")
-    print(target_img.size())
-    print(recon_img.size())
+    infer_cls = InferVAE()
+    first_img = infer_cls.sample_image(3, "mnist")
+    second_img = infer_cls.sample_image(7, "mnist")
+    int_img = infer_cls.interpolate(first_img, second_img, "ConvVAE", 2, "mnist")
+    print(int_img.size())
--- src/deploy/streamlit_app.py
+++ src/deploy/streamlit_app.py
@@ -1,40 +1,104 @@
 """Streamlit App
 """
 import streamlit as st
+import torch
 
 import config
-from deploy.inference import Inference
+from dataloaders import dataloaders
+from deploy.inference import InferVAE
 from models import models
 
+inferer = InferVAE()
+
 st.set_page_config("VAE_GAN")
-st.title("Variational AutoEncoder (VAE) and Generative Adversarial Network (GAN)")
 
-mnist = str(config.DEPLOY_PATH / "images" / "mnist.png")
-fmnist = str(config.DEPLOY_PATH / "images" / "fmnist.png")
-cifar = str(config.DEPLOY_PATH / "images" / "cifar10.png")
+data_imgs = {
+    "mnist": str(config.DEPLOY_PATH / "images" / "mnist.png"),
+    "fmnist": str(config.DEPLOY_PATH / "images" / "fmnist.png"),
+    "cifar10": str(config.DEPLOY_PATH / "images" / "cifar10.png"),
+}
 
-st.image(
-    [mnist, fmnist, cifar], width=220, caption=["Mnist", "Fashion Mnist", "CIFAR-10"]
+st.title(":blue[VAE] and :green[GAN] on MNIST, Fashion MNIST and CIFAR-10")
+
+col1_recon_settings, col2_recon_settings, col3_recon_settings = st.columns(3)
+
+with st.sidebar:
+    dataset = st.selectbox("Dataset", ["mnist", "fmnist", "cifar10"])
+    _, input_size = dataloaders[dataset]
+    with st.expander("See sample images"):
+        st.image(data_imgs[dataset])
+    hidden_size = st.radio("Hidden Size", [2, 32, 64, 128])
+    model_type = st.radio("Model", models.keys())
+
+tab1, tab2, tab3 = st.tabs(
+    ["Image Generation", "Image Reconstruction", "Image Interpolation"]
 )
 
-inferer = Inference()
+### Generation
+with tab1:
+    st.header("Image Generation")
+    st.markdown(
+        "Generate a random sample from Standard Gaussian Distribution from the Latent Space"
+    )
+    with st.form("Image Gen Form"):
+        gen_random = st.form_submit_button("Generate Image")
+        if gen_random:
+            z = torch.randn(hidden_size)
+            st.write(z)
+            recon_img = inferer.decode(z, model_type, hidden_size, dataset)
+            st.image(
+                recon_img.permute(1, 2, 0).detach().numpy(),
+                width=400,
+                caption="Reconstructed Image",
+            )
 
-dataset = st.selectbox("Dataset", ["mnist", "fmnist", "cifar10"])
-hidden_size = st.radio("Hidden Size", [2, 32, 64, 128])
-model_type = st.radio("Model", models.keys())
+### Reconstruction
+with tab2:
+    st.header("Image Reconstruction")
+    recon_class_no = st.selectbox("Digit", list(range(10)))
+    target_col, recon_col = st.columns(2)
 
-class_no = st.selectbox("Digit", list(range(10)))
+    with st.form("Image Recon Form"):
+        sample_img_recon = st.form_submit_button("Reconstruct Image")
+        if sample_img_recon:
+            with target_col:
+                target_img = inferer.sample_image(recon_class_no, dataset)
+                st.image(
+                    target_img.permute(1, 2, 0).detach().numpy(),
+                    width=300,
+                    caption="Original Image",
+                )
 
-target_img, recon_img = inferer.reconstruction(
-    class_no, model_type, hidden_size, dataset
-)
+            with recon_col:
+                recon_img = inferer.reconstruction(
+                    target_img, model_type, hidden_size, dataset
+                )
+                st.image(
+                    recon_img.permute(1, 2, 0).detach().numpy(),
+                    width=300,
+                    caption="Reconstructed Image",
+                )
 
-st.image(
-    [
-        target_img.permute(1, 2, 0).detach().numpy(),
-        recon_img.permute(1, 2, 0).detach().numpy(),
-    ],
-    caption=["Original Image", "Reconstructed Image"],
-)
+with tab3:
+    st.header("Image Interpolation")
+    img1, img2 = st.columns(2)
 
-# streamlit run /src/deploy/streamlit_app.py
+    with img1:
+        digit_class1 = st.selectbox("First Digit", list(range(10)))
+        first_img = inferer.sample_image(digit_class1, dataset)
+        st.image(first_img.permute(1, 2, 0).detach().numpy(), width=300)
+
+    with img2:
+        digit_class2 = st.selectbox("Second Digit", list(range(10)))
+        second_img = inferer.sample_image(digit_class2, dataset)
+        st.image(second_img.permute(1, 2, 0).detach().numpy(), width=300)
+
+    with st.form("Image Interpolate Form"):
+        interpolate_submit = st.form_submit_button("Interpolate Image")
+        if interpolate_submit:
+            intermediate_images = inferer.interpolate(
+                first_img, second_img, model_type, hidden_size, dataset
+            )
+            st.image(intermediate_images.permute(1, 2, 0).detach().numpy(), width=650)
+
+# streamlit run ./src/deploy/streamlit_app.py
