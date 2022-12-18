e2b6d0dae0fbc1a5952b418ba338d8f623946ff9
Le Quan <99384454+benjaminlq@users.noreply.github.com>
2022-12-18 19:11:37 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -9,7 +9,7 @@ for DATA in "mnist" "fmnist" "cifar10"
                 for MODEL in "BaseVAE" "DeepVAE" "ConvVAE"
                     do
                         echo "Train $MODEL model on $DATA dataset using hidden size $HIDDEN for $1 epochs"
-                        python3 ./src/train.py --model $MODEL --hidden $HIDDEN -e $1 --dataset $DATA
+                       # python3 ./src/train.py --model $MODEL --hidden $HIDDEN -e $1 --dataset $DATA
                     done
             done
     done
