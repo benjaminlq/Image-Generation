0fd027199358b74ad69f28587e61522b8dcbfcfe
lequan2902 <benjamin.lequan@gmail.com>
2022-12-18 11:18:18 +0800

    Ckpt

--- train.sh
+++ train.sh
@@ -3,12 +3,13 @@
 echo "Start Running Training Bash Script"
 
 for DATA in "fmnist" "cifar10"
-    for HIDDEN in 2 32 64 128
-        do
-            for MODEL in "BaseVAE" "DeepVAE" "ConvVAE"
-                do
-                    echo "Train $MODEL model on $DATA dataset using hidden size $HIDDEN"
-                    python3 ./src/train.py --model $MODEL --hidden $HIDDEN -e 1 --dataset $DATA
-                done
-        done
-done
+    do
+        for HIDDEN in 2 32 64 128
+            do
+                for MODEL in "BaseVAE" "DeepVAE" "ConvVAE"
+                    do
+                        echo "Train $MODEL model on $DATA dataset using hidden size $HIDDEN"
+                        python3 ./src/train.py --model $MODEL --hidden $HIDDEN -e 25 --dataset $DATA
+                    done
+            done
+    done
